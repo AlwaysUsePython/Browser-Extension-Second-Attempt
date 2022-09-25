8f8dbc12d0c9fd9c19b6c729e35c10be0b3c20c3
Elliot Lichtman <elliot.joseph.lichtman@gmail.com>
2022-09-24 17:19:08 -0700

    Make everything purple >:)

--- content.js
+++ content.js
@@ -1,1 +1,6 @@
-console.log("Chrome Extension ready to go!")
+console.log("Noticed HW")
+
+let paragraphs = document.getElementsByTagName('p');
+for (elt of paragraphs){
+    elt.style['background-color'] = '#FF00FF';
+} 
